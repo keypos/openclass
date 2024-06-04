dcc66a2ceb6ddd2ded6bc6d2e5fc441abfa06d9a
kp <k4ypos@proton.me>
2024-06-04 08:40:37 +0000

    student search

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -11,9 +11,27 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/students")
-def get_all_students():
-    query = text("SELECT * FROM student")
-    result = db.session.execute(query)
+def students():
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    grade = request.args.get('grade')
+    
+    query = "SELECT * FROM student WHERE 1=1"
+    params = {}
+    
+    if first_name:
+        query += " AND first_name LIKE :first_name"
+        params['first_name'] = f"%{first_name}%"
+    
+    if last_name:
+        query += " AND last_name LIKE :last_name"
+        params['last_name'] = f"%{last_name}%"
+    
+    if grade:
+        query += " AND grade = :grade"
+        params['grade'] = grade
+    
+    result = db.session.execute(text(query), params)
     
     students = []
     for row in result:
